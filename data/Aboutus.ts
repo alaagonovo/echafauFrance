43b3c03927c3ef947ac2fc8dfc35d947f8d59b97
alaagonovo <alaa@gonovo.tech>
2025-03-10 17:53:41 +0200

    space

--- data/Aboutus.ts
+++ data/Aboutus.ts
@@ -4,12 +4,12 @@ const aboutUsData = [
   {
     icon: "/svgs/global.svg",
     color: "var(--dark-red)",
-    desc: `Echafau, avec ${expYears} ans d'excellence à travers l'Europe, apporte désormais fièrement ses solutions d'échafaudage innovantes à Paris. En utilisant des matériaux 100 % métalliques, nous garantissons des approches durables, résistantes et à la pointe de la construction moderne.`,
+    desc: `Echafau, avec ${expYears} ans d'excellence à travers l'Europe, apporte désormais fièrement ses solutions d'échafaudage innovantes à Paris. En utilisant des matériaux 100% métalliques, nous garantissons des approches durables, résistantes et à la pointe de la construction moderne.`,
   },
   {
     icon: "/svgs/secure.svg",
     color: "#32CD32",
-    desc: `La sécurité a toujours été notre pierre angulaire. Depuis ${expYears} ans, nous avons fièrement maintenu un taux d’accidents de 0 % grâce à notre planification minutieuse et à nos procédures rigoureuses, de l’assemblage au démontage.`,
+    desc: `La sécurité a toujours été notre pierre angulaire. Depuis ${expYears} ans, nous avons fièrement maintenu un taux d’accidents de 0% grâce à notre planification minutieuse et à nos procédures rigoureuses, de l’assemblage au démontage.`,
   },
   {
     icon: "/svgs/power.svg",
