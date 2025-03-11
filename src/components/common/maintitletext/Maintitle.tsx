acbfe407cc51411fc88162709bc32f89a77b21d6
alaagonovo <alaa@gonovo.tech>
2025-03-11 13:48:19 +0200

    end aboutus changes

--- src/components/common/maintitletext/Maintitle.tsx
+++ src/components/common/maintitletext/Maintitle.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Maintitle({ title, bio }: { title: string; bio: string }) {
   return (
     <div className="mb-14 max-w-[900px] mx-auto lg:px-0 px-4">
-      <h1 className="text-[var(--dark-blue)] text-4xl sm:text-6xl font-semibold text-center mb-4">
+      <h1 className="text-[var(--dark-blue)] text-5xl sm:text-6xl font-semibold text-center mb-4">
         {title}
       </h1>
       <p className="text-center text-[var(--main-red)] italic leading-5 font-medium text-[12px] sm:text-base">
