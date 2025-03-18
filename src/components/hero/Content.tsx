390c0bdd48290f9a70e562297d2941ecf7688ab6
alaagonovo <alaa@gonovo.tech>
2025-03-18 10:53:39 +0200

    update font size in hero section

--- src/components/hero/Content.tsx
+++ src/components/hero/Content.tsx
@@ -2,9 +2,9 @@ import RoundButton from "../common/roundbutton/RoundButton";
 
 function Content() {
   return (
-    <div className="w-[94%] lg:max-w-[800px] md:max-w-[700px] absolute -translate-y-1/2 left-[4%] top-1/2 h-[65vh] flex flex-col justify-between min-h-[490px]">
+    <div className="w-[94%] lg:max-w-[888px] md:max-w-[700px] absolute -translate-y-1/2 left-[4%] top-[calc(50%+45px)] h-[65vh] flex flex-col justify-between min-h-[450px]">
       {/* Animated H1 */}
-      <h1 className="text-white font-medium leading-12 sm:leading-14 md:leading-[65px] md:text-5xl sm:text-4xl text-3xl">
+      <h1 className="text-white leading-12 sm:leading-14 md:leading-[65px] md:text-5xl sm:text-4xl text-3xl">
         Découvrez nos solutions d&apos;échafaudage conçues par des experts.
         Robustesse et sécurité garanties.
       </h1>
