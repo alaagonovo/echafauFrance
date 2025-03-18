f89aa59842682e2d148e749a8f075b8bfed4f487
alaagonovo <alaa@gonovo.tech>
2025-03-18 13:03:25 +0200

    edit about us

--- src/components/hero/Content.tsx
+++ src/components/hero/Content.tsx
@@ -3,13 +3,13 @@ import RoundButton from "../common/roundbutton/RoundButton";
 function Content() {
   return (
     <div className="w-[94%] lg:max-w-[888px] md:max-w-[700px] absolute -translate-y-1/2 left-[4%] top-[calc(50%+45px)] h-[65vh] flex flex-col justify-between min-h-[450px]">
-      {/* Animated H1 */}
+      
       <h1 className="text-white leading-12 sm:leading-14 md:leading-[65px] md:text-5xl sm:text-4xl text-3xl">
         Découvrez nos solutions d&apos;échafaudage conçues par des experts.
         Robustesse et sécurité garanties.
       </h1>
 
-      {/* Animated P (Starts AFTER H1 with a manual delay) */}
+     
 
       <p className="text-[18px] md:text-[22px] font-light sm:font-normal text-white w-[80%]">
         Des services fiables, sécurisés et professionnels pour répondre
--- src/components/sections/aboutus/AboutUs.tsx
+++ src/components/sections/aboutus/AboutUs.tsx
@@ -1,41 +1,61 @@
 import React from "react";
 import aboutUsData from "../../../../data/Aboutus";
 import Image from "next/image";
-import Maintitle from "@/components/common/maintitletext/Maintitle";
+import RoundButton from "@/components/common/roundbutton/RoundButton";
+// import Maintitle from "@/components/common/maintitletext/Maintitle";
 
 function AboutUs() {
   return (
     <section className="mt-16  mx-auto max-w-[1380px]" id="aproposdenous">
-      <Maintitle
+      {/* <Maintitle
         title="À propos de nous"
         bio="Echafau, fort de 18 ans d'excellence en Europe, offre des solutions d’échafaudage 100% métalliques, durables et innovantes. Avec un taux d’accidents de 0%, nous garantissons sécurité et fiabilité à chaque projet."
         setPadding={true}
-      />
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-[.9fr_1.1fr] gap-12 w-full lg:w-[94%] xl:w-[92%] mx-auto ">
-        <div className="relative w-full rounded-0 lg:rounded-2xl overflow-hidden lg:h-full sm:h-[650px] h-[450px]">
-          <Image
-            src="/assets/aboutview.jpg"
-            alt="about Image view"
-            fill
-            style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
-            loading="lazy"
-          />
+      /> */}
+      <p className="w-[92%] mx-auto text-center font-medium py-8 text-[var(--dark-red)]">
+        À Propos
+      </p>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-[.9fr_1.1fr] gap-22 w-full lg:w-[94%] xl:w-[92%] mx-auto">
+        <div className=" flex flex-col gap-6">
+          <div
+            className="relative w-full rounded-0  overflow-hidden max-w-[470px] lg:h-full h-[450px] mx-auto"
+            style={{
+              clipPath:
+                "polygon(0 0, 84% 0, 100% 16%, 100% 80%, 100% 100%, 0 100%, 0% 80%, 0% 20%)",
+              boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+            }}
+          >
+            <Image
+              src="/assets/aboutview.jpg"
+              alt="about Image view"
+              fill
+              loading="lazy"
+            />
+          </div>
+          {/* <p className="py-4 font-medium text-xl">
+            Echafau est une entreprise française fondée en 2006.
+          </p> */}
+          {/* <RoundButton name="Obtenir un devis" path="/" /> */}
         </div>
+
         <div className="grid gap-8 grid-cols-1 lg:px-0 px-4">
+          <h1 className="font-semibold text-3xl text-[var(--dark-red)]">
+            Echafau est une entreprise française fondée en 2006.
+          </h1>
           {aboutUsData.map((item, index) => (
-            <div key={index} className=" flex flex-col  gap-2">
+            <div key={index} className="flex   gap-2">
               <div
-                className="relative w-[80px] h-[80px] rounded-3xl p-4 flex items-center justify-center"
-                style={{
-                  backgroundColor: `${item.color}`,
-                  boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
-                }}
+                className="relative w-[24px] h-[24px] flex-shrink-0"
+                style={
+                  {
+                    // backgroundColor: `${item.color}`,
+                    // boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+                  }
+                }
               >
-                <Image src={item.icon} width={64} height={64} alt="icon" />
+                <Image src={item.icon} fill alt="icon" />
               </div>
-              <p className="text-[18px] text-[var(--text-color)]">
-                {item.desc}
-              </p>
+              <p className="text-xl">{item.desc}</p>
             </div>
           ))}
         </div>
