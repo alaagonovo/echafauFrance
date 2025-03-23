7b3e3cd56f39faedbb2f97d4c2e720c7285fa387
alaagonovo <alaa@gonovo.tech>
2025-03-23 17:58:53 +0200

    end review section large screen

--- src/components/sections/feedback/ReviewsSection.tsx
+++ src/components/sections/feedback/ReviewsSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 
 // import styles from "./reviews.module.css";
 // import ReviewCard from "./reviewcard/ReviewCard";
