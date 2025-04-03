3cccf52606880b39f7ea6ebe2b69fb131c349804
alaagonovo <alaa@gonovo.tech>
2025-04-03 13:58:15 +0200

    adding some changes

--- data/servicesicons.ts
+++ data/servicesicons.ts
@@ -1,18 +1,23 @@
 const ServicesIconData = [
   {
     typeName: "Démontage échafaudage",
+    icon: "/svgs/scafflding1.svg",
   },
   {
     typeName: "Montage échafaudage",
+    icon: "/svgs/scafflding2white.svg",
   },
   {
     typeName: "Plan échafaudage",
+    icon: "/svgs/scafflding3.svg",
   },
   {
     typeName: "Note de calcul",
+    icon: "/svgs/note.svg",
   },
   {
     typeName: "Transport",
+    icon: "/svgs/transwhite.svg",
   },
 ];
 
--- src/components/hero/ServicesIcon.tsx
+++ src/components/hero/ServicesIcon.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import ServicesIconData from "../../../data/servicesicons";
 import styles from "./servicesicon.module.css";
+import Image from "next/image";
 function ServicesIcon() {
   return (
-    <div className="gap-4 flex flex-col justify-end items-end absolute right-0 bottom-12 sm:bottom-24">
+    <div className="gap-4 flex flex-col justify-end items-end absolute right-0 bottom-12 sm:bottom-24 overflow-hidden">
       {ServicesIconData.map((serv, index) => (
         <div
           key={index}
-          className={`flex justify-center items-center bg-[rgba(0,38,82,0.95)] py-4 px-2 lg:p-4 ${styles.main_cont_icon}`}
+          className={`flex justify-center items-center bg-[#131f2d] p-3 lg:py-4 lg:px-5   ${styles.main_cont_icon}`}
           data-aos="fade-up"
           data-aos-delay={`${(index + 1) * 200}`}
         >
           <div className="relative shrink-0">
-            <p className="text-gray-400 font-medium pe-2 me-2 border-r-2">
-              0{index + 1}
+            <p className="text-gray-400 font-medium pe-2 me-2 border-r-3 border-[#070f1e] relative">
+              <span
+                className={`${styles.num_span} text-xs -top-2 right-0 relative`}
+              >
+                {" "}
+                0{index + 1}
+              </span>
+              <span
+                className={`${styles.icon_span} relative w-8 h-8 lg:hidden block`}
+              >
+                <Image src={serv.icon} alt="service icon" fill />
+              </span>
             </p>
           </div>
-          <h2 className="text-gray-400 text-sm lg:text-lg">{serv.typeName}</h2>
+          <h2 className="text-gray-400 text-sm font-semibold">
+            {serv.typeName}
+          </h2>
         </div>
       ))}
     </div>
