b73aa096df4fe7d440e4c7a060449367329d0812
alaagonovo <alaa@gonovo.tech>
2025-04-03 17:00:33 +0200

    adding some changes on text hero

--- src/components/hero/Content.tsx
+++ src/components/hero/Content.tsx
@@ -2,9 +2,9 @@ import RoundButton from "../common/roundbutton/RoundButton";
 
 function Content() {
   return (
-    <div className="w-[94%] lg:max-w-[888px] md:max-w-[700px] absolute -translate-y-1/2 left-[4%] top-[calc(50%+35px)] sm:top-[calc(50%+65px)] h-[70vh] sm:h-[55vh] lg:h-[40vh] flex flex-col justify-between min-h-[320px]">
+    <div className="w-[94%] sm:w-[90%] md:w-[94%] md:max-w-[700px] xl:max-w-[888px]  absolute -translate-y-1/2 left-[4%] top-[calc(50%+35px)] sm:top-[calc(50%+65px)] h-[70vh] sm:h-[55vh] lg:h-[40vh] flex flex-col justify-between min-h-[320px]">
       <h1
-        className="text-white leading-12 sm:leading-14 md:leading-[50px] md:text-[34px] sm:text-4xl text-4xl"
+        className="text-white leading-12 sm:leading-14 md:leading-[50px]  sm:text-4xl text-[32px]"
         data-aos="fade-up"
       >
         Découvrez nos solutions d&apos;échafaudage conçues par des experts.
@@ -12,7 +12,7 @@ function Content() {
       </h1>
 
       <p
-        className="text-[18px] md:text-[22px] font-light sm:font-normal text-white w-[80%]"
+        className="text-[22px] font-light sm:font-normal text-white w-[80%] sm:w-[80%]"
         data-aos="fade-up"
         data-aos-delay="200"
       >
