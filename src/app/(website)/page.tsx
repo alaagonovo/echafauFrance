9abf66c143ad0f301f87615102f5bd61681c0346
alaagonovo <alaa@gonovo.tech>
2025-06-25 21:47:14 +0300

    replace two sections

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
   return (
     <>
       <MainHero />
-      <AboutUs />
       <Services />
+      <AboutUs />
       <Ournumbers />
       <RecentProjects />
       <Reviews />
